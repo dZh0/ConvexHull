d62917d9f006ec74bec00c1e966f72981d719202
dZh0 <30321850+dZh0@users.noreply.github.com>
2017-09-15 18:55:03 +0300

    Update

---
+++ CnvH.h
@@ -0,0 +1,39 @@
+// Convex Hull of vector combinations
+
+#pragma once
+//true dependancies
+#include <vector>
+#include <map>
+#include "FVector.h"
+
+class CnvH {
+public:
+
+// Internal structures:
+	enum geometry {empty, linear, planar, volume};
+
+	// Holds a vector and a weight map from colection that constructs it
+	struct point {
+		FVector vec;
+		std::map<int,float> weight;
+
+		//constructors
+		point() : vec(FV_ZERO) {};
+		point(FVector _vec, int _idx, float _power = 1.0f) : vec(_vec) { weight[_idx] = _power; }; 
+	};
+	struct quad {
+		// Holds the 4 indecies of points thatmake up the quad and a normal vector
+		size_t pointIdx[4];
+		FVector normal;
+	};
+
+	CnvH();											// Default constructor
+	CnvH(FVector const* p_arr, const int _size);	// Array constructor
+	void add(FVector const* p_vec, const int idx);
+	
+private:
+	geometry state;									// Geometric state: linear, planar etc.
+	std::vector<FVector const*> collection;			// Colection of references TODO: set template class container
+	std::vector<point> points;						// Points of the Convex Hull (INDEXED CONTAINER!)
+	std::vector<quad> quads;						// Quads of the Convex Hull
+};
